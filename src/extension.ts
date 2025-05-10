1c1db5e73752a71644b1121380df13d092c56f3d
menh <dinhthienmenh1505@gmail.com>
2025-05-10 11:34:13 +0700

    feat(API): init storage singleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
 import * as vscode from 'vscode';
 import { ViewProvider } from './providers/view.provider';
-import { StorageProvider } from './providers/storage.provider';
+import { initializeStorageProvider } from './providers/storage.provider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	 // Initialize the StorageProvider singleton
+	initializeStorageProvider(context);
+	
 	const viewProvider = new ViewProvider(context);
-	const storageProvider = new StorageProvider(context);
+	
 	context.subscriptions.push(vscode.window.registerWebviewViewProvider(ViewProvider.viewType, viewProvider));
 
 }
