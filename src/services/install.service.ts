f4589c4b73919c0356281bc56b827ec71fbc08d3
menh <dinhthienmenh1505@gmail.com>
2025-05-10 10:38:20 +0700

    test check os version function

---
+++ src/services/install.service.ts
@@ -0,0 +1,34 @@
+import * as os from 'os';
+
+enum OS {
+    MAC = 1, // darwin
+    WINDOWS = 2, // windows
+    UBUNTU = 3, // ubuntu
+    OTHER = 4, // debian
+}
+
+function getOS(): OS {
+    const platform = os.platform();
+    if (platform === 'linux') {
+        try {
+            const { execSync } = require('child_process');
+            const distro = execSync('hostnamectl --json=short').toString().trim();
+            if (distro.KernelVersion.toString().includes('Ubuntu')) {
+                return OS.UBUNTU;
+            } else {
+                return OS.OTHER;
+            }
+        }
+        catch {
+            return OS.OTHER;
+        }
+    } else if (platform.toString().includes('win')) {
+        return OS.WINDOWS;
+    } else if (platform === 'darwin') {
+        return OS.MAC;
+    } else {
+        return OS.OTHER;
+    }
+}
+
+console.log('Current OS:', getOS());
